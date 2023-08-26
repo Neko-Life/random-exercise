aec90ed156a4c3249d5b3355b1c72a52eec028af
Neko-Life <nekolife123579@gmail.com>
2023-08-26 11:34:07 +0700

    add poll usage example

--- main-pipe.cpp
+++ main-pipe.cpp
@@ -1,7 +1,9 @@
 #include <assert.h>
 #include <fcntl.h>
 #include <iostream>
+#include <poll.h>
 #include <string>
+#include <sys/prctl.h>
 #include <sys/wait.h>
 #include <thread>
 #include <unistd.h>
@@ -33,11 +35,48 @@ std::string getCmd() {
           std::to_string(volume / (float)100) + "\"" + " -f s16le " OUT_CMD);
 }
 
+int run_child(int pwritefd, int creadfd, int preadfd, int cwritefd) {
+  if (prctl(PR_SET_PDEATHSIG, SIGINT) == -1) {
+    perror("child");
+    exit(1);
+  }
+
+  close(pwritefd); /* Reader will see EOF */
+  close(preadfd);
+
+  int status;
+  /* fprintf(stderr, "creadfd: %d\n", creadfd); */
+  /* fprintf(stderr, "cwritefd: %d\n", cwritefd); */
+
+  status = dup2(creadfd, STDIN_FILENO);
+  close(creadfd);
+  status = dup2(cwritefd, STDOUT_FILENO);
+  close(cwritefd);
+
+  if (status == -1) {
+    perror("child");
+    _exit(status);
+  }
+
+  /* int dnull = open("/dev/null", O_WRONLY); */
+  /* dup2(dnull, STDERR_FILENO); */
+  /* close(dnull); */
+
+  execlp("ffmpeg", "ffmpeg", "-f", "s16le", "-i", "-", "-af",
+         (std::string("volume=") + std::to_string(volume / (float)100)).c_str(),
+         "-f", "s16le", "-bufsize", "64K", /*"-preset", "ultrafast",*/ OUT_CMD,
+         (char *)NULL);
+
+  perror("!");
+  _exit(EXIT_FAILURE);
+}
+
 int main() {
   std::thread t(listener);
   t.detach();
 
-  FILE *input = popen((std::string("opusdec ") + MUSIC_FILE " -").c_str(), "r");
+  FILE *input = popen(
+      (std::string("opusdec ") + MUSIC_FILE " - 2>/dev/null").c_str(), "r");
 
   if (!input) {
     fprintf(stderr, "popen\n");
@@ -46,61 +85,35 @@ int main() {
 
   if (pipe(ppipefd) == -1) {
     perror("ppipe");
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
-
   int preadfd = ppipefd[0];
   int cwritefd = ppipefd[1];
 
   if (pipe(cpipefd) == -1) {
     perror("cpipe");
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
-
   int creadfd = cpipefd[0];
   int pwritefd = cpipefd[1];
 
   cpid = fork();
   if (cpid == -1) {
     perror("fork");
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
   if (cpid == 0) { /* Child reads from pipe */
-    close(preadfd);
-    close(pwritefd); /* Reader will see EOF */
-
-    int status;
-    /* fprintf(stderr, "creadfd: %d\n", creadfd); */
-    /* fprintf(stderr, "cwritefd: %d\n", cwritefd); */
-
-    status = dup2(creadfd, STDIN_FILENO);
-    close(creadfd);
-    status = dup2(cwritefd, STDOUT_FILENO);
-    close(cwritefd); /* Reader will see EOF */
-
-    if (status == -1) {
-      perror("child");
-      _exit(status);
-    }
-
-    execlp(
-        "ffmpeg", "ffmpeg", "-f", "s16le", "-rtbufsize", "4096", "-i", "-",
-        "-af",
-        (std::string("volume=") + std::to_string(volume / (float)100)).c_str(),
-        "-f", "s16le", OUT_CMD, (char *)NULL);
-
-    perror("!");
-    _exit(EXIT_FAILURE);
+    run_child(pwritefd, creadfd, preadfd, cwritefd);
   } else {           /* Parent writes argv[1] to pipe */
-    close(cwritefd); /* Reader will see EOF */
     close(creadfd);  /* Close unused read end */
+    close(cwritefd); /* Close unused read end */
 
     /* fcntl(pwritefd, F_SETFL, fcntl(pwritefd, F_GETFL, 0) | O_NONBLOCK); */
     /* fcntl(preadfd, F_SETFL, fcntl(preadfd, F_GETFL, 0) | O_NONBLOCK); */
 
-    FILE *preadfile = fdopen(preadfd, "r");
     FILE *pwritefile = fdopen(pwritefd, "w");
+    FILE *preadfile = fdopen(preadfd, "r");
 
     size_t total_written_size = 0;
     float current_volume = volume;
@@ -118,37 +131,67 @@ int main() {
       /*   ; */
       /* total_written_size += written_size; */
 
-      /* while ((read_size = fread(buffer, 1, BUFFER_SIZE, preadfile)) > 0) */
-      /*   fwrite(buffer, 1, read_size, stdout); */
+      int can_read = poll(, , 0);
+
+      if (can_read > 0)
+        while ((read_size = fread(buffer, 1, BUFFER_SIZE, preadfile)) > 0)
+          fwrite(buffer, 1, read_size, stdout);
 
       if (volume != current_volume) {
         /* pclose(out); */
         /* out = nullptr; */
-        fclose(preadfile);  /* Close unused read end */
         fclose(pwritefile); /* Reader will see EOF */
         pwritefile = NULL;
-        preadfile = NULL;
 
-        break;
-        /* out = popen(getCmd().c_str(), "w"); */
+        int status;
+        waitpid(cpid, &status, 0); /* Wait for child */
+
+        fprintf(stderr, "child status: %d\n", status);
+
+        // kill child
+        kill(cpid, SIGINT);
+
+        if (pipe(ppipefd) == -1) {
+          perror("ppipe");
+          break;
+        }
+        preadfd = ppipefd[0];
+        cwritefd = ppipefd[1];
+
+        if (pipe(cpipefd) == -1) {
+          perror("cpipe");
+          break;
+        }
+        creadfd = cpipefd[0];
+        pwritefd = cpipefd[1];
+
+        cpid = fork();
+        if (cpid == -1) {
+          close(creadfd);
+          close(pwritefd);
+          close(cwritefd);
+          close(preadfd);
+          perror("fork");
+          break;
+        }
+
+        if (cpid == 0) { /* Child reads from pipe */
+          return run_child(pwritefd, creadfd, preadfd, cwritefd);
+        }
+
+        close(creadfd);  /* Close unused read end */
+        close(cwritefd); /* Close unused read end */
+        pwritefile = fdopen(pwritefd, "w");
+
+        assert(fwrite(buffer, 1, read_size, pwritefile) == read_size);
 
         current_volume = volume;
       }
     }
 
-    /* while ((read_size = fread(buffer, 1, BUFFER_SIZE, preadfile)) > 0) */
-    /*   fwrite(buffer, 1, read_size, stdout); */
-
     if (pwritefile)
       fclose(pwritefile); /* Reader will see EOF */
-    if (preadfile)
-      fclose(preadfile); /* Close unused read end */
-    close(pwritefd);     /* Reader will see EOF */
-    close(preadfd);      /* Close unused read end */
-
-    printf("waiting for child\n");
-    wait(NULL); /* Wait for child */
-    exit(EXIT_SUCCESS);
+    /* close(pwritefd); */
   }
 
   /* out = popen(getCmd().c_str(), "w"); */
@@ -162,6 +205,13 @@ int main() {
   pclose(input);
   /* pclose(out); */
   fprintf(stderr, "closed\n");
+  fprintf(stderr, "waiting for child\n");
+
+  // kill child
+  kill(cpid, SIGINT);
+  int status;
+  waitpid(cpid, &status, 0); /* Wait for child */
+  fprintf(stderr, "child status: %d\n", status);
 
   running = false;
 
