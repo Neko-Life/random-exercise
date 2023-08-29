9e87e78376f517197c63ba7aff91b1b700501881
Neko-Life <nekolife123579@gmail.com>
2023-08-30 02:50:15 +0700

    optimization!! no longer need 30% cpu to run!

--- main-pipe.cpp
+++ main-pipe.cpp
@@ -1,5 +1,6 @@
 /*
     Great exercise, now i know how i will implement effects for Musicat
+    Very great exercise!! So much fun learning how this works!
     !TODO: explore more on usage of different effects on ffmpeg
 */
 #include <assert.h>
@@ -15,7 +16,14 @@
 #include <thread>
 #include <unistd.h>
 
+// BUFFER_SIZE%READ_CHUNK_SIZE should be 0 to guarantee
+// complete read write cycle, knowing chunk size
+// reduce cpu usage greatly as reading byte by byte
+// is REALLY cpu intensive
 #define BUFFER_SIZE processing_buffer_size
+// found out ffmpeg default write chunk to stdout
+// this can also be interpreted as BUFSIZ/2
+#define READ_CHUNK_SIZE 4096
 
 #define OUT_CMD "pipe:1"
 /* #define OUT_CMD "tcp://localhost:8080/listen" */
@@ -104,6 +112,7 @@ static int run_child(int pwritefd, int creadfd, int preadfd, int cwritefd) {
   }
 
   // redirect ffmpeg stderr to /dev/null
+  // commented for debugging purpose
   /* int dnull = open("/dev/null", O_WRONLY); */
   /* dup2(dnull, STDERR_FILENO); */
   /* close(dnull); */
@@ -119,6 +128,8 @@ static int run_child(int pwritefd, int creadfd, int preadfd, int cwritefd) {
 }
 
 int main() {
+  // this is BAD!! Never create a thread before doing any fork!
+  // Rather always fork than mixing multi-threading with forking
   std::thread t(listener);
   t.detach();
 
@@ -207,10 +218,12 @@ int main() {
     // know if the write/read will be blocked in the middle of operation
     // (waiting for the exact requested amount of space/data to be available)
     // if we were to do more than a byte at a time
+    //
+    // as safe chunk read size is known we no longer need to read byte by byte
 
     // we can do chunk writing here as as long as the output keeps being read,
     // the buffer will keep being consumed by ffmpeg, as long as it's not too
-    // big
+    // big it won't block to wait for buffer space
     int write_has_event = poll(pwfds, 1, 0);
     bool write_ready = (write_has_event > 0) && (pwfds[0].revents & POLLOUT);
     while (write_ready &&
@@ -235,14 +248,36 @@ int main() {
     if (read_ready) {
       int read_count = 0;
 
-      uint8_t out_buffer;
+      // with known chunk size that always guarantee correct read size that
+      // empties ffmpeg buffer, poll will report correctly that the pipe
+      // is really empty so we know exactly when to stop reading, this is
+      // where it's problematic when BUFFER_SIZE%READ_CHUNK_SIZE doesn't equal 0
+      uint8_t out_buffer[BUFFER_SIZE];
       while (read_ready &&
-             ((input_read_size = read(preadfd, &out_buffer, 1)) > 0)) {
-        fwrite(&out_buffer, 1, input_read_size, stdout);
+             ((input_read_size += read(preadfd, out_buffer + input_read_size,
+                                       READ_CHUNK_SIZE)) > 0)) {
+
+        // write only when buffer is full
+        if (input_read_size == BUFFER_SIZE) {
+          size_t written_out = 0;
+          while ((written_out += fwrite(out_buffer + written_out, 1,
+                                        input_read_size - written_out,
+                                        stdout)) < input_read_size)
+            ;
+          fprintf(stderr, "Written to stdout: %ld\n", input_read_size);
+          input_read_size = 0;
+        }
 
         read_has_event = poll(prfds, 1, 0);
         read_ready = (read_has_event > 0) && (prfds[0].revents & POLLIN);
       }
+
+      // empties the last buffer that usually size less than BUFFER_SIZE
+      // which expected to be always left with 0 remainder when divided by
+      // READ_CHUNK_SIZE
+      fprintf(stderr, "Current last written to stdout: %ld\n", input_read_size);
+      if (input_read_size > 0)
+        fwrite(out_buffer, 1, input_read_size, stdout);
     }
 
     // recreate ffmpeg process to update filter chain
@@ -251,11 +286,15 @@ int main() {
       // eof without worrying about polling and blocked read
       close(pwritefd);
 
-      uint8_t rest_buffer[BUFFER_SIZE];
-      // read the rest of data before closing current instance
-      while ((input_read_size = read(preadfd, rest_buffer, BUFFER_SIZE)) > 0) {
-        fwrite(buffer, 1, input_read_size, stdout);
-      }
+      // this is no longer needed as the buffer is guaranteed empty after above
+      // operation, but should be checked and emptied when the main loop breaks
+      //
+      // uint8_t rest_buffer[BUFFER_SIZE];
+      // // read the rest of data before closing current instance
+      // while ((input_read_size = read(preadfd, rest_buffer, BUFFER_SIZE)) > 0)
+      // {
+      //   fwrite(buffer, 1, input_read_size, stdout);
+      // }
 
       // close read fd
       close(preadfd);
@@ -268,8 +307,10 @@ int main() {
       waitpid(cpid, &status, 0);
       fprintf(stderr, "child status: %d\n", status);
 
+      // we don't need to kill here as ffmpeg will exit
+      // when the write end (its stdin) of pipe is closed
       // kill child
-      kill(cpid, SIGTERM);
+      // kill(cpid, SIGTERM);
 
       // wait for child until it completely died to prevent it becoming zombie
       waitpid(cpid, &status, 0);
@@ -285,7 +326,8 @@ int main() {
       cwritefd = ppipefd[1];
 
       if (pipe(cpipefd) == -1) {
-        // fds will be closed on exit
+        // fds will be closed on program exit
+        close(cwritefd);
         perror("cpipe");
         break;
       }
@@ -295,6 +337,10 @@ int main() {
       cpid = fork();
       if (cpid == -1) {
         // fds will be closed on exit
+        // i know we should close
+        // child's pipes here
+        close(cwritefd);
+        close(creadfd);
         perror("fork");
         break;
       }
@@ -319,28 +365,37 @@ int main() {
   }
 
   // exiting, clean up
+  // close reader stdout
   fclose(input);
   input = NULL;
 
-  if (creadfd > -1)
-    close(creadfd);
-  if (pwritefd > -1)
-    close(pwritefd);
-  if (cwritefd > -1)
-    close(cwritefd);
-  if (preadfd > -1)
-    close(preadfd);
-
-  creadfd = -1;
+  // close ffmpeg stdin
+  // acknowledge that its stdout still have
+  // some data to read which should be read
+  // immediately after closing its stdin
+  close(pwritefd);
   pwritefd = -1;
-  cwritefd = -1;
+
+  size_t last_read_size = 0;
+  uint8_t rest_buffer[BUFFER_SIZE];
+  // read the rest of data before closing ffmpeg stdout
+  while ((last_read_size = read(preadfd, rest_buffer, BUFFER_SIZE)) > 0) {
+    fwrite(buffer, 1, last_read_size, stdout);
+  }
+
+  // data have been read entirely
+  // close it
+  close(preadfd);
   preadfd = -1;
 
   fprintf(stderr, "fds closed\n");
 
+  // we don't NEED to do this!
+  // the whole program should exit correctly now without broken pipe error
+  // yayy!!
   // kill child
-  kill(cpid, SIGTERM);
-  kill(rpid, SIGTERM);
+  // kill(cpid, SIGTERM);
+  // kill(rpid, SIGTERM);
 
   // wait for childs to make sure they're dead and
   // prevent them to become zombies
